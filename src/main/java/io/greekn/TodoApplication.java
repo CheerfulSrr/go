28986d952ea7b1638faba7725564c5f32b4b2bb5
greekn <cheerfuls@outlook.com>
2020-11-07 23:07:50 +0800

    spring-back

--- src/main/java/io/greekn/TodoApplication.java
+++ src/main/java/io/greekn/TodoApplication.java
@@ -2,7 +2,6 @@ package io.greekn;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * <p>
@@ -18,7 +17,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  * @copyright: Copyright (c) 2020
  */
 @SpringBootApplication
-@EnableDiscoveryClient
 public class TodoApplication {
     public static void main(String[] args) {
         SpringApplication.run(TodoApplication.class);
